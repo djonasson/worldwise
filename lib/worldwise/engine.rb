9eaf4ae3ffcfc47b2eb1cef95a475a8423d392ac
Daniel Jonasson <daniel@guadeo.com>
2013-02-02 16:05:12 +0100

    Generate rspec tests instead of test unit

--- lib/worldwise/engine.rb
+++ lib/worldwise/engine.rb
@@ -1,5 +1,14 @@
 module Worldwise
   class Engine < ::Rails::Engine
     #isolate_namespace Worldwise
+    config.generators do |g|
+      #g.template_engine :haml
+      g.integration_tool :rspec
+      g.test_framework :rspec, view_specs: false
+      #g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.orm :active_record
+      #g.stylesheets true
+      #g.form_builder :simple_form
+    end
   end
 end
